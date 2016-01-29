3c6e3747fbe80a033454d509eaca5a663df7c405
Joshua Guatafson <silversevenfox@yahoo.com>
2016-01-29 08:46:19 -0800

    update first spec

--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,9 @@
 describe('Pizza', function() {
   it("create a pizza with all of a pizza's properties", function() {
     var newPizza = new Pizza('Deep Dish', 'Large', 'Fresh Monzzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
-    expect(newPizza.type).to.equal('Superman');
-    expect(newPizza.size).to.equal('Morning');
-    expect(newPizza.cheese).to.equal('Adult');
+    expect(newPizza.type).to.equal('Deep Dish');
+    expect(newPizza.size).to.equal('Large');
+    expect(newPizza.cheese).to.equal('Fresh Monzzarella');
     expect(newPizza.toppingOne).to.equal('Pepperoni');
     expect(newPizza.toppingTwo).to.equal('Bacon');
     expect(newPizza.toppingThree).to.equal('Green Peppers');
