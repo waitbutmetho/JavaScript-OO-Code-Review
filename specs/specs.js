c2c3ff95c0e8cd4062aa83b6847800c89bbb6cd4
Joshua Guatafson <silversevenfox@yahoo.com>
2016-01-29 10:11:48 -0800

    add spec for toppingTwoCharge

--- specs/specs.js
+++ specs/specs.js
@@ -9,20 +9,24 @@ describe('Pizza', function() {
     expect(testPizza.toppingThree).to.equal('Green Peppers');
     expect(testPizza.price).to.equal();
   });
-  it("adds the typeCharge method to all tickets", function() {
+  it("adds the typeCharge method to all pizza's", function() {
     var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Mozzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
     expect(testPizza.typeCharge()).to.equal(2);
   });
-  it("adds the sizeCharge method to all tickets", function() {
+  it("adds the sizeCharge method to all pizza's", function() {
     var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Mozzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
     expect(testPizza.sizeCharge()).to.equal(8);
   });
-  it("adds the cheeseCharge method to all tickets", function() {
+  it("adds the cheeseCharge method to all pizza's", function() {
     var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Mozzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
     expect(testPizza.cheeseCharge()).to.equal(1);
   });
-  it("adds the toppingOneCharge method to all tickets", function() {
+  it("adds the toppingOneCharge method to all pizza's", function() {
     var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Mozzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
     expect(testPizza.toppingOneCharge()).to.equal(1);
+  });
+  it("adds the toppingTwoCharge method to all pizza's", function() {
+    var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Mozzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
+    expect(testPizza.toppingTwoCharge()).to.equal(1);
   });
 });
