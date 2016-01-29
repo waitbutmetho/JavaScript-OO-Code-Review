2cef30af13d18f366d62ed30bf34e79eaa16bc7a
Joshua Guatafson <silversevenfox@yahoo.com>
2016-01-29 08:48:44 -0800

    write code for Pizza constuctor passed spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+function Pizza(type, size, cheese, toppingOne, toppingTwo, toppingThree) {
+this.type = type;
+this.size = size
+this.cheese = cheese;
+this.toppingOne = toppingOne;
+this.toppingTwo = toppingTwo;
+this.toppingThree = toppingThree;
+}
