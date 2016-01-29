2905d8ee576226c87a2dbb661d3d7b93daa01634
Joshua Guatafson <silversevenfox@yahoo.com>
2016-01-29 09:21:17 -0800

    add second spec for typeCharge method

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function Pizza(type, size, cheese, toppingOne, toppingTwo, toppingThree) {
 this.type = type;
-this.size = size
+this.size = size;
 this.cheese = cheese;
 this.toppingOne = toppingOne;
 this.toppingTwo = toppingTwo;
--- specs/specs.js
+++ specs/specs.js
@@ -1,12 +1,16 @@
 describe('Pizza', function() {
   it("create a pizza with all of a pizza's properties", function() {
-    var newPizza = new Pizza('Deep Dish', 'Large', 'Fresh Monzzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
-    expect(newPizza.type).to.equal('Deep Dish');
-    expect(newPizza.size).to.equal('Large');
-    expect(newPizza.cheese).to.equal('Fresh Monzzarella');
-    expect(newPizza.toppingOne).to.equal('Pepperoni');
-    expect(newPizza.toppingTwo).to.equal('Bacon');
-    expect(newPizza.toppingThree).to.equal('Green Peppers');
-    expect(newPizza.price).to.equal();
+    var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Monzzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
+    expect(testPizza.type).to.equal('Deep Dish');
+    expect(testPizza.size).to.equal('Large');
+    expect(testPizza.cheese).to.equal('Fresh Monzzarella');
+    expect(testPizza.toppingOne).to.equal('Pepperoni');
+    expect(testPizza.toppingTwo).to.equal('Bacon');
+    expect(testPizza.toppingThree).to.equal('Green Peppers');
+    expect(testPizza.price).to.equal();
+  });
+  it("adds the typeCharge method to all tickets", function() {
+    var testPizza = new Pizza('Deep Dish', 'Large', 'Fresh Monzzarella', 'Pepperoni', 'Bacon', 'Green Peppers');
+    expect(testPizza.typeCharge()).to.equal(2);
   });
 });
